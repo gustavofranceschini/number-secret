f2cc1b21c7f178f0f3a5a344789984903873587c
Gustavo Franceschini <gustavofranceschini22@gmail.com>
2023-03-13 18:01:53 -0300

    Created script.JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
+// Declaração dos valores
 const menorValor = 1;
 const maiorValor = 1000;
-
 
 const elementoMenorValor = document.getElementById('menor-valor');
 elementoMenorValor.innerHTML = menorValor;
@@ -8,27 +8,29 @@ const elementoMaiorValor = document.getElementById('maior-valor');
 elementoMaiorValor.innerHTML = maiorValor;
 
 
+const buttonSearch = document.getElementById('input-validation');
+const inputValue = document.getElementById('input-value-number')
 
-// const botaoProcurar = document.getElementById('procurar');
-// const campoTexto = document.getElementById('input-teste');
-
-// // Gerando o número aleatorio
-// const numeroVariavel = Math.floor(100 * Math.random());
-// console.log(numeroVariavel)
-
+// Gerando o numero aleatorio para ser encontrado
+const numberVariavel = Math.floor((1000 + 1) * Math.random());
+console.log(numberVariavel)
 
 
-// botaoProcurar.addEventListener('click', () => {
-//     const resultado = campoTexto.value;
-//     verificarNumeroCorretoOuIncorreto(resultado);
-//     campoTexto.value = "";
-// })
+buttonSearch.addEventListener('click', () => {
+    const resultado = inputValue.value;
+    verificarNumeroCorretoOuIncorreto(resultado);
+    inputValue.value = "";
+})
 
-
-// function verificarNumeroCorretoOuIncorreto(resultado) {
-//     if (resultado == numeroVariavel) {
-//         console.log('Numero correto')
-//     } else {
-//         console.log('Numero errado')
-//     }
-// }
+function verificarNumeroCorretoOuIncorreto(resultado) {
+    if (resultado == numberVariavel) {
+        document.body.innerHTML = 
+        `
+        <div>
+            <h1>Parabens você acertou!</h1>
+        </div>
+        `
+    } else {
+        console.log('Numero errado')
+    }
+}
