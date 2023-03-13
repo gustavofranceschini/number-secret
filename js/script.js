8a86edf8f35cc5916ea1488caa5537b038d994a8
Gustavo Franceschini de Araujo <gustavofranceschini22@gmail.com>
2023-03-13 14:44:56 -0300

    Created project Number Secret - HTML, CSS and JS

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const menorValor = 1;
+const maiorValor = 1000;
+
+
+const elementoMenorValor = document.getElementById('menor-valor');
+elementoMenorValor.innerHTML = menorValor;
+const elementoMaiorValor = document.getElementById('maior-valor');
+elementoMaiorValor.innerHTML = maiorValor;
+
+
+
+// const botaoProcurar = document.getElementById('procurar');
+// const campoTexto = document.getElementById('input-teste');
+
+// // Gerando o número aleatorio
+// const numeroVariavel = Math.floor(100 * Math.random());
+// console.log(numeroVariavel)
+
+
+
+// botaoProcurar.addEventListener('click', () => {
+//     const resultado = campoTexto.value;
+//     verificarNumeroCorretoOuIncorreto(resultado);
+//     campoTexto.value = "";
+// })
+
+
+// function verificarNumeroCorretoOuIncorreto(resultado) {
+//     if (resultado == numeroVariavel) {
+//         console.log('Numero correto')
+//     } else {
+//         console.log('Numero errado')
+//     }
+// }
